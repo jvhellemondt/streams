5b1608ff83cde7d0dd5846df671a8cc2f4aeeeb4
jvhellemondt <jens.vanhellemondt@gmail.com>
2019-10-22 22:55:13 +0200

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as RRouter, Route } from 'react-router-dom';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
+
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <RRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/show" component={StreamShow} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+        </div>
+      </RRouter>
+    </div>
+  );
+};
+
+
+export default App;
